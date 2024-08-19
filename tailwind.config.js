735cdafc3bcc0836bf8707ff15b59735d45ebaaf
QuocAnh189 <anhquoc18092003@gmail.com>
2024-08-19 23:50:29 +0700

    config tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,8 @@ export default {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('tailwind-scrollbar')({ nocompatible: true }),
+    require('daisyui')
+  ]
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import svgrPlugin from 'vite-plugin-svgr'
+import macrosPlugin from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), svgrPlugin(), macrosPlugin()],
+  define: {
+    'process.env': process.env
+  }
 })
